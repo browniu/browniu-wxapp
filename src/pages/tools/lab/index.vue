9ff487d9ae2738942a526ab4bf6021378f4cfd60
brown <niuniusoga@qq.com>
2018-09-11 23:45:22 +0800

    cloud: get json

--- src/pages/tools/lab/index.vue
+++ src/pages/tools/lab/index.vue
@@ -9,27 +9,47 @@
 
 <script>
 export default {
+  data () {
+    return {
+      openID: null
+    }
+  },
   methods: {
     apitest () {
       wx.vibrateLong()
-      wx.showToast({
-        title: '禁制に入る',
-        icon: 'none',
-        duration: 2000
-      })
-      wx.cloud.callFunction({
-        name: 'login',
-        data: {},
+
+      const db = wx.cloud.database()
+      // 查询当前用户所有的 counters
+      db.collection('realView').get({
         success: res => {
-          console.log('[云函数] [login] user openid: ', res.result.openid)
+          console.log(res.data)
+          this.setData({
+            queryResult: JSON.stringify(res.data, null, 2)
+          })
+          console.log('[数据库] [查询记录] 成功: ', res)
         },
         fail: err => {
-          console.error('[云函数] [login] 调用失败', err)
+          wx.showToast({
+            icon: 'none',
+            title: '查询记录失败'
+          })
+          console.error('[数据库] [查询记录] 失败：', err)
         }
       })
     }
   },
   mounted () {
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {},
+      success: res => {
+        console.log('[云函数] [login] user openid: ', res.result.openid)
+        this.openID = res.result.openid
+      },
+      fail: err => {
+        console.error('[云函数] [login] 调用失败', err)
+      }
+    })
     wx.setNavigationBarTitle({
       title: '实验室'
     })
