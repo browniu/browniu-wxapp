23b703443cb0deca3e046371f93fcbd56b190f18
brown <niuniusoga@qq.com>
2018-10-10 00:06:26 +0800

    引入外部字体

--- src/pages/tools/weather/index.vue
+++ src/pages/tools/weather/index.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>Hello Vue!{{currentData.date}}</div>
+  <div class="weather">Hello Vue!{{currentData.date}}
+    <div class="test">
+      万里无云
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,7 +12,8 @@ export default {
   data () {
     return {
       currentData: ['currentCity', 'date', 'pm25', 'temperature', 'weatherDesc', 'wind'],
-      originalData: ['currentCity', ['des', 'tipt', 'title', 'zs'], 'pm25', ['date', 'dayPictureUrl', 'nightPictureUrl', 'temperature', 'weather', 'wind']]
+      originalData: ['currentCity', ['des', 'tipt', 'title', 'zs'], 'pm25', ['date', 'dayPictureUrl', 'nightPictureUrl', 'temperature', 'weather', 'wind']],
+      currentTemprature: ''
     }
   },
   onLoad: function () {
@@ -20,6 +25,7 @@ export default {
         console.log(res)
         this.currentData = res.currentWeather[0]
         this.originalData = res.originalData.results[0]
+        this.currentTemprature = this.currentData.date.split('：')[1].split('℃')[0]
       }
     })
   }
@@ -27,4 +33,19 @@ export default {
 </script>
 <style lang='stylus' scoped>
 @import '../../../assets/styles/index.styl'
+@font-face {
+  font-family 'qingke'
+  src url('http://pb85uax7t.bkt.clouddn.com/fzqingke.TTF') format('truetype')
+  /* iOS 4.1- */
+}
+.weather {
+  color co_3
+  font-family serif
+  .test {
+    font-family qingke
+    font-weight 500
+    font-size 4em
+    font-size 100px
+  }
+}
 </style>
