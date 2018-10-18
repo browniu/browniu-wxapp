a7555153d50d47b2a9e2683384995b012960f114
browniu <niuniusoga@qq.com>
2018-10-18 20:10:07 +0800

    成功设置背景音乐

--- miniGame/js/main.js
+++ miniGame/js/main.js
@@ -1,9 +1,20 @@
-import Music from 'music'
+// audio
+const innerAudioContext = wx.createInnerAudioContext()
+innerAudioContext.autoplay = false
+innerAudioContext.src = 'http://pb85uax7t.bkt.clouddn.com/QNXT.mp3'
+innerAudioContext.onPlay(() => {
+  console.log('music go')
+})
+innerAudioContext.onError((res) => {
+  console.log(res.errMsg)
+  console.log(res.errCode)
+})
+
 // canvas
-console.log('xixixi')
+console.log('canvas init')
 var canvas = wx.createCanvas()
 
-console.log(canvas.width, canvas.height)
+console.log('canvasSize:', canvas.width, canvas.height)
 
 var context = canvas.getContext('2d')
 context.fillStyle = 'brown'
@@ -11,6 +22,55 @@ context.fillRect(0, 0, 200, 200)
 
 export default class Main {
   restart() {
-    this.music = new Music()
+    console.log('gun')
+  }
+
+  enemyGenerate() {
+    if (databus.frame % 30 === 0) {
+      let enemy = databus.pool.getItemByClass('enemy', Enemy)
+      enemy.init(6)
+      databus.enemys.push(enemy)
+    }
+  }
+
+  /**
+   * canvas重绘函数
+   * 每一帧重新绘制所有的需要展示的元素
+   */
+  render() {
+    console.log('render')
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+    this.bg.render(ctx)
+
+    databus.bullets
+      .concat(databus.enemys)
+      .forEach((item) => {
+        item.drawToCanvas(ctx)
+      })
+
+    this.player.drawToCanvas(ctx)
+
+    databus.animations.forEach((ani) => {
+      if (ani.isPlaying) {
+        ani.aniRender(ctx)
+      }
+    })
+
+    this.gameinfo.renderGameScore(ctx, databus.score)
+
+  }
+
+  // 实现游戏帧循环
+  loop() {
+    databus.frame++
+
+    this.update()
+    this.render()
+
+    this.aniId = window.requestAnimationFrame(
+      this.bindLoop,
+      canvas
+    )
   }
 }
--- miniGame/js/music.js
+++ miniGame/js/music.js
@@ -1,8 +1,29 @@
+// let instance
+
+// /**
+//  * 统一的音效管理器
+//  */
+// export default class Music {
+//   constructor() {
+//     if (instance)
+//       return instance
+
+//     instance = this
+
+//     this.bgmAudio = new Audio()
+//     this.bgmAudio.loop = true
+//     // this.bgmAudio.src = 'http://pb85uax7t.bkt.clouddn.com/QNXT.mp3'
+//     this.bgmAudio.src = 'bgm.mp3'
+
+//     this.playBgm()
+//   }
+
+//   playBgm() {
+//     this.bgmAudio.play()
+//   }
+// }
 let instance
 
-/**
- * 统一的音效管理器
- */
 export default class Music {
   constructor() {
     if (instance)
@@ -12,8 +33,7 @@ export default class Music {
 
     this.bgmAudio = new Audio()
     this.bgmAudio.loop = true
-    // this.bgmAudio.src = 'http://pb85uax7t.bkt.clouddn.com/QNXT.mp3'
-    this.bgmAudio.src = 'bgm.mp3'
+    this.bgmAudio.src = 'http://pb85uax7t.bkt.clouddn.com/QNXT.mp3'
 
     this.playBgm()
   }
