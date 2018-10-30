56ccbae1dca2d0cce4df7555b1e0cc236cb93838
browniu <niuniusoga@qq.com>
2018-10-30 17:17:57 +0800

    flappy bird 基本完成 明天优化一下就可以上线了

--- src/pages/tools/lab/index.vue
+++ src/pages/tools/lab/index.vue
@@ -5,14 +5,24 @@
       <div class="inner">
         <div :class="['sky',running?'act':'']">
           <div class="playground">
-            <div class="hero" :style="{top:heroPosition+'px'}"></div>
+            <div v-show="running" class="hero" :style="{top:heroPosition+'px',transform:'translateY(-50%) rotate('+heroRotation+'deg)'}"></div>
+            <div :class="['pipes',pipesStatus?'act':'']">
+              <div v-if="index>pipesIndex" class="item" v-for="(item, index) in pipes" :key="index">
+                <div class="top" :style="{height:(item*10)+'%'}"></div>
+                <div class="bottom" :style="{height:((level-item)*10)+'%'}"></div>
+              </div>
+            </div>
           </div>
         </div>
-        <div :class="['land',running?'act':'']"></div>
+        <div :class="['land',running?'act':'']">
+          <div class="scope">
+            <div :style="{backgroundImage:'url(http://pb85uax7t.bkt.clouddn.com/f_'+item+'.png)'}" class="item" v-for="(item, index) in scopeData" :key="index"></div>
+          </div>
+        </div>
       </div>
     </div>
     <div v-if="!running" class="panel" @click="run">
-      <div class="start">start</div>
+      <!-- <div class="start">go</div> -->
     </div>
   </div>
 </template>
@@ -28,9 +38,25 @@ export default {
       sourceCount: 0,
       loaded: false,
       running: false,
-      innerAudioContext: {},
+      music: [0, 0, 0],
       heroPosition: 200,
-      velocity: 0
+      heroRotation: 0,
+      heroStatus: 0,
+      velocity: 0,
+      timer: {},
+      pipes: [],
+      pipesGen: {},
+      pipesDes: {},
+      pipesDelate: {},
+      pipesIndex: 0,
+      pipesStatus: -3,
+      pipeTop: 0,
+      pipeBot: 0,
+      k: 0,
+      scope: 0,
+      scopeData: [],
+      viewHeight: wx.getSystemInfoSync().windowHeight,
+      level: 8
     }
   },
   methods: {
@@ -48,27 +74,105 @@ export default {
     run () {
       if (this.running) {
         this.running = false
+        this.reset()
       } else {
         this.running = true
         let gravity = 0.25
-        setInterval(() => {
+        this.scopeShow(0)
+        this.timer = setInterval(() => {
+          this.k++
+          this.heroStatus = (this.heroPosition / this.viewHeight) * 10
+          // 坠毁地面
+          if (this.heroStatus > 8) {
+            this.over()
+            return
+          }
+          // 碰撞区间
+          if (this.k > 75 && this.k < 90 && this.pipesStatus > 0) {
+            if (this.heroStatus < this.pipeBot || this.heroStatus > this.pipeTop) {
+              console.log('dead')
+              this.over()
+              return
+            }
+            // console.log('k:' + this.k, 'pipesStatus:' + this.pipesStatus, 'heroStatus:' + this.heroStatus, 'pipeBottom:' + this.pipeBot, 'pipeTop:' + this.pipeTop)
+            if (this.k === 80) {
+              this.scope = this.scope + 1
+              this.scopeShow(this.scope)
+            }
+          }
+
           this.velocity += gravity
+          this.heroRotation = Math.min((this.velocity / 20) * 90, 90)
           if (this.velocity < 20) {
             this.heroPosition += this.velocity
           }
         }, 1000.0 / 60.0)
+        this.pipesGen = setInterval(() => {
+          this.pipesStatus++
+          this.pipeBot = Math.round(this.pipes[this.pipesStatus])
+          this.pipeTop = this.pipeBot + 2.5
+          this.pipes.push((Math.random() * 3 + 2).toFixed(0))
+          // console.log(this.pipes, this.pipesIndex)
+          // this.pipesStatus = false
+          this.k = 0
+        }, 1500)
+        this.pipesDelate = setTimeout(() => {
+          this.pipesDes = setInterval(() => {
+            this.pipesIndex++
+            // this.pipes.splice(0, 1)
+            // this.pipesStatus = true
+          }, 1500)
+        }, 7500)
       }
     },
+    reset () {
+      this.velocity = 0
+      this.running = false
+      this.heroPosition = 200
+      this.heroRotation = 0
+      this.pipes = []
+      this.pipesStatus = -3
+      this.pipesIndex = 0
+      this.scope = 0
+      // this.scopeShow(this.scope)
+      console.log('reset')
+      clearInterval(this.timer)
+      clearInterval(this.pipesGen)
+      clearInterval(this.pipesDes)
+      clearTimeout(this.pipesDelate)
+    },
     fly () {
-      console.log('fly')
-      // this.innerAudioContext.play()
-      this.velocity = -4.6
+      this.music[0].play()
+      this.velocity = -4.5
+    },
+    over () {
+      this.music[2].play()
+      this.running = true
+      this.run()
+    },
+    musicLoader () {
+      this.music[0] = wx.createInnerAudioContext()
+      this.music[0].src = 'http://pb85uax7t.bkt.clouddn.com/sfx_wing.mp3'
+      this.music[1] = wx.createInnerAudioContext()
+      this.music[1].src = 'http://pb85uax7t.bkt.clouddn.com/sfx_point.ogg'
+      this.music[2] = wx.createInnerAudioContext()
+      this.music[2].src = 'http://pb85uax7t.bkt.clouddn.com/sfx_die.ogg'
+    },
+    scopeShow (s) {
+      this.music[1].play()
+      let scopes = s.toString()
+      this.scopeData = []
+      console.log(scopes)
+      for (let i = 0; i < scopes.length; i++) {
+        this.scopeData.push(scopes[i])
+      }
     }
   },
+  onShow () {
+    this.over()
+  },
   onLoad () {
-    this.innerAudioContext = wx.createInnerAudioContext()
-    this.innerAudioContext.autoplay = false
-    this.innerAudioContext.src = 'http://pb85uax7t.bkt.clouddn.com/sfx_wing.mp3'
+    this.musicLoader()
   }
 }
 </script>
@@ -92,15 +196,74 @@ export default {
         animation-play-state running
       }
       .playground {
+        height 100%
         .hero {
           position absolute
           top 50%
-          left 50%
-          transform translate(-50%, -50%)
+          left 45%
           background-image url('../../../assets/images/duck/bird.png')
           height 24px
           width 34px
           animation animBird 300ms steps(4) infinite
+        }
+        .pipes {
+          height 100%
+          white-space nowrap
+          .item {
+            height 100%
+            width 50px
+            display inline-block
+            position absolute
+            left 0
+            animation animPipe 6000ms linear infinite
+            &:last-child {
+              left -100px
+            }
+            &.act {
+              left -100px
+            }
+            & .top {
+              position absolute
+              top 0
+              left 0
+              width 100%
+              background-image url('../../../assets/images/duck/pipe.png')
+              background-repeat repeat-y
+              background-size 100% auto
+              background-position 0 0
+              &:before {
+                height 30px
+                width 100%
+                display inline-block
+                position absolute
+                bottom -5px
+                left 0
+                content ''
+                background-image url('../../../assets/images/duck/pipe-down.png')
+                background-repeat no-repeat
+                background-size 100% auto
+              }
+            }
+            & .bottom {
+              position absolute
+              bottom 0
+              width 100%
+              background #000
+              background-image url('../../../assets/images/duck/pipe.png')
+              &:before {
+                height 30px
+                width 103%
+                display inline-block
+                position absolute
+                top -5px
+                left 0
+                content ''
+                background-image url('../../../assets/images/duck/pipe-up.png')
+                background-repeat no-repeat
+                background-size 100% auto
+              }
+            }
+          }
         }
       }
     }
@@ -117,19 +280,38 @@ export default {
       &.act {
         animation-play-state running
       }
+      .scope {
+        text-align center
+        position relative
+        top 50%
+        transform translateY(-30%)
+        & .item {
+          background-image url('../../../assets/images/duck/f_0.png')
+          background-repeat no-repeat
+          background-size contain
+          display inline-block
+          background-position center
+          height 50px
+          width 45px
+        }
+      }
     }
   }
   .panel {
     position absolute
     top 50%
     left 50%
-    transform translate(-50%, -50%)
+    transform translate(-50%, -80%)
     z-index 100
-    background #fff
     padding 10px 25px
     border-radius 3px
     font-size 14px
     color co_2
+    background-image url('../../../../static/duck/splash.png')
+    height 28vh
+    width 28vh
+    background-repeat no-repeat
+    background-size contain
   }
 }
 </style>
